4eaf01a10771170743f583d57107c748957bbcea
timwang <weifanwang16@gmail.com>
2021-10-09 20:48:53 +0800

    update

--- Models/__init__.py
+++ Models/__init__.py
@@ -3,15 +3,5 @@
 #from Models.Linear_Navi import Linear_Navi
 #from Models.place_cells import PlaceCells
 #import Models.RNNLIF import RNNLIF
-import Models.RNNLIF
-
+#import Models.RNNLIF
 from Models.PlaceCells2D import PlaceCells2D
-
-def BuildModule(module):
-    if module.type in ["LeakyNonLinearLayer"]:
-        # to be implemented
-        pass
-    else:
-        raise utils_torch.model.BuildModule(module)
-
-
--- main.py
+++ main.py
@@ -1,31 +1,22 @@
-
-# run this script to do different tasks
-# python main.py --task copy --path ./Instances/TP-1/  //move necessary files for training and analysis to path.
-# Every component, such as model, optimizer, trainer, agent, is initialized according to a dict in a .py file.
-# run this script to do different tasks
-# python main.py --task copy --path ./Instances/TP-1/  //move necessary files for training and analysis to path.
-# Every component, such as model, optimizer, trainer, agent, is initialized according to a dict in a .py file.
-
-import os
 from re import L
 import sys
 import argparse
 import traceback
 
-#from utils.utils import ArgsGlobal
-
 parser = argparse.ArgumentParser()
-parser.add_argument("task", nargs="?", default="ProcessTasks")
+parser.add_argument("task", nargs="?", default="DoTasksFromFile")
 parser.add_argument("-IsDebug", default=True)
 Args = parser.parse_args()
+
+TaskFilePath = "./task.jsonc"
 
 def main():
     if Args.task in ["CleanLog", "CleanLog", "cleanlog"]:
         CleanLog()
     elif Args.task in ["CleanFigure"]:
         CleanFigures()
-    elif Args.task in ["ProcessTasks"]:
-        TaskObj = utils_torch.LoadTaskFile()
+    elif Args.task in ["DoTasksFromFile"]:
+        TaskObj = utils_torch.LoadTaskFile(TaskFilePath)
         TaskList = utils_torch.ParseTaskObj(TaskObj)
         if not Args.IsDebug:
             try: # catch all unhandled exceptions
@@ -43,16 +34,13 @@ def main():
 def ScanConfigFile(FilePath="./config.jsonc"):
     import json5
     with open(FilePath, "r") as f:
-        JsonObj = json5.load(f) # json5 allows comments
-    Config = JsonObj
-    sys.path.append(Config["LibraryPath"]["utils_torch"])
+        config = json5.load(f) # json5 allows comments. config is either dict or list.
+    sys.path.append(config["Library"]["utils_torch"]["IncludePath"])
     import utils_torch
     import utils
-    utils_torch.attrs.SetAttrs(utils.ArgsGlobal, "Config", Config)
+    utils_torch.attrs.SetAttrs(utils.ArgsGlobal, "config", utils_torch.PyObj(config)) # mount config on utils_torch.ArgsGlobal.config
 ScanConfigFile()
-
 import utils
-from utils import ArgsGlobal
 utils.Init()
 
 import utils_torch
@@ -60,13 +48,10 @@ from utils_torch.attrs import *
 utils_torch.SetArgsGlobal(utils.ArgsGlobal)
 
 def CleanLog():
-    import utils_torch
     utils_torch.files.RemoveAllFilesAndDirs("./log/")
 
 def CleanFigures():
-    import utils_torch
     utils_torch.files.RemoveMatchedFiles("./", r".*\.png")
 
 if __name__=="__main__":
     main()
-
